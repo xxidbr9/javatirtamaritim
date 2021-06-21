dd57260a03f3f00fb29f567d364f0cfbad70d081
Barnando Akbarto Hidayatullah <barnando13@gmail.com>
2021-06-21 23:07:58 +0700

    feat(base): add navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { FiPhone, FiMail } from "react-icons/fi";
+
+const MiniText = (props) => <span className="pl-3">{props.children}</span>;
+
+const TopNavbar = (props) => {
+  return (
+    <nav className="w-full bg-black">
+      <div className="container flex flex-row-reverse items-center text-white text-sm h-6 font-medium m-auto">
+        <div className="flex items-center">
+          <FiMail />
+          <MiniText>Javatirtamaritim@gmail.com</MiniText>
+        </div>
+        <div className="flex items-center mr-5">
+          <FiPhone />
+          <MiniText>+62 778 7490 805</MiniText>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+const BottomNavbar = (props) => {
+  return (
+    <nav className="w-full bg-white h-16">
+      <div className="container h-full m-auto flex justify-between items-center">
+        <span className="uppercase font-bold text-2xl">JAVA TIRTA</span>
+        <ul className="flex flex-row gap-10 font-medium">
+          <li className="text-red-500">Home</li>
+          <li>Service</li>
+          <li>Galery</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 const Navbar = () => {
-    return (
-        <nav className="w-full bg-pink-200 ">
-         <div className="container flex flex-row-reverse">
-             ok
-         </div>
-        </nav>
-    )
-}
+  const ref = useRef(null);
+  useEffect(() => {
+    console.log(ref.current.getBoundingClientRect());
+  }, [ref]);
+  return (
+    <nav className="shadow-md fixed w-full" ref={ref}>
+      <TopNavbar />
+      <BottomNavbar />
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
---
+++ src/context/Navbar.context.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavbarProvider = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NavbarProvider
