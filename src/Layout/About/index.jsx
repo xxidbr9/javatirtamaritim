bd83ff96419ba37b44c257d49dcb07fa6a668596
Andhana Utama <magicwarms@gmail.com>
2021-06-27 15:17:26 +0700

    feat(Change about page content): Change about page content

--- src/Layout/About/index.jsx
+++ src/Layout/About/index.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
-import Hero from '../../components/Hero'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import Hero from "../../components/Hero";
 import ImageBanner from "../../assets/images/international_trade.jpg";
-
 
 // const AccentSmall = (props) => {
 //   return (
@@ -35,45 +34,37 @@ const AboutLayout = () => {
       <Navbar active="/about" />
       <Hero title={"About Us"} ImageBanner={ImageBanner} />
       <section className="tablet:px-8 mobile:px-4 desktop:px-0">
-
         <div className="tablet:container mx-auto bg-white relative my-20 grid mobile:grid-cols-1 laptop:grid-cols-2 gap-12">
           <div class="py-16 laptop:pl-6 tablet:col-span-2 tablet:text-center">
-            <Title>
-              Mission
-            </Title>
+            <Title>Motto</Title>
             <div>
-              <Paragraph>
-                The purpose of the historical establishment of the company/organization is aimed at the needs of marine transportation modes in Indonesia which are increasingly popular with the increasing need for services in the industrial or mining sector in Indonesia..
-              </Paragraph>
+              <Paragraph>One stop service and Stop solution</Paragraph>
             </div>
           </div>
           <div class="py-16">
-            <Title>
-              Vision
-            </Title>
+            <Title>Vision</Title>
             <div>
               <Paragraph>
-                The purpose of the historical establishment of the company/organization is aimed at the needs of marine transportation modes in Indonesia which are increasingly popular with the increasing need for services in the industrial or mining sector in Indonesia..
+                To provide the best quality in every service in the shipping
+                world to get a sense of security and comfort
               </Paragraph>
             </div>
           </div>
           <div class="py-16 laptop:pl-6">
-            <Title>
-              Motto
-            </Title>
+            <Title>Mission</Title>
             <div>
               <Paragraph>
-                The purpose of the historical establishment of the company/organization is aimed at the needs of marine transportation modes in Indonesia which are increasingly popular with the increasing need for services in the industrial or mining sector in Indonesia..
+                Uphold the value of high commitment in realizing trust for the
+                benefit of customers and customer satisfaction
               </Paragraph>
             </div>
           </div>
-
         </div>
       </section>
 
       <Footer />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default AboutLayout
+export default AboutLayout;
